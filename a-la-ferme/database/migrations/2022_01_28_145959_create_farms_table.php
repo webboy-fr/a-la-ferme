cbe770d6a6e55496b5d505cf7b11b6fee8cb1c66
Aiiisaka <theptitegeek@gmail.com>
2022-01-28 16:29:10 +0100

    Adding Migrations

---
+++ a-la-ferme/database/migrations/2022_01_28_145959_create_farms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFarmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('farms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('day')->nullable();
+            $table->timestamp('opening');
+            $table->timestamp('ending');
+            $table->integer('farmer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('farms');
+    }
+}
